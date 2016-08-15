118b740921eadcad6b8e85d68f250e0bed58893e
Scott Diallo <diallot@me.com>
2016-08-15 13:55:34 -0700

    controller added

---
+++ tests/controllerSpec.js
@@ -0,0 +1,20 @@
+describe('countriesController', function () {
+
+    var controller = null;
+    $scope = null;
+
+    beforeEach(function () {
+        module('ccApp');
+    });
+
+    beforeEach(inject(function ($controller, $rootScope) {
+        $scope = $rootScope.$new();
+        controller = $controller('countriesController', {
+            $scope: $scope
+        });
+    }));
+
+    it('initial predicate value should equal country name'function () {
+        expect($scope.predicate).toEqual("countryName");
+    });
+});
